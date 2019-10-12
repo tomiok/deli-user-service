bbb11d0d9873b8797418afdcd1a51e1e22771903
tomaslingotti <tomaslingotti@gmail.com>
2019-10-12 17:04:21 -0300

    add middleware for logging

--- cmd/web/main/routes.go
+++ cmd/web/main/routes.go
@@ -4,8 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/deli/user-service/commons"
+	"github.com/deli/user-service/commons/logs"
 	"github.com/deli/user-service/engine"
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/docgen"
 	"net/http"
 	"runtime"
@@ -13,15 +15,18 @@ import (
 )
 
 func Routes(e engine.Spec, router *chi.Mux) {
-	router.Route("/users", func(r chi.Router) {
-		r.Get("/{userId}", func(w http.ResponseWriter, r *http.Request) {
+
+	router.Route("/users", func(rt chi.Router) {
+		rt.Use(middleware.Logger)
+
+		rt.Get("/{userId}", func(w http.ResponseWriter, r *http.Request) {
 			getUSerByIdHandler(e, w, r)
 		})
-		r.Post("/aw", func(w http.ResponseWriter, r *http.Request) {
+		rt.Post("/aw", func(w http.ResponseWriter, r *http.Request) {
 			createsAdminOrWriterHandler(e, w, r)
 		})
 
-		r.Post("/login", func(w http.ResponseWriter, r *http.Request) {
+		rt.Post("/login", func(w http.ResponseWriter, r *http.Request) {
 			validateUserHandler(e, w, r, encrypt)
 		})
 	})
@@ -54,4 +59,12 @@ func encrypt(r *http.Request) (string, string) {
 	_ = json.NewDecoder(r.Body).Decode(&cred)
 
 	return cred.Username, commons.EncryptPass(cred.Password)
+}
+
+func customLogger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		logs.Info("Executing middlewareOne")
+		next.ServeHTTP(w, r)
+		logs.Info("Executing middlewareOne again")
+	})
 }
